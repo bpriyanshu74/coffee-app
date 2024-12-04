14cd0002fa4c81e295a6dcd4f9fd0997c65bd4cb
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2024-12-04 11:26:37 +0530

    folder structure setup

---
+++ Backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/home", (req, res) => {
+    res.send(`hi from port ${process.env.PORT}`);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("listening on port 3001");
+});
+export { app };
